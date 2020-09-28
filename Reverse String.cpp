44843e3b9e7ab88b6c8c1d5566d24d9a738de25a
mohand reda sakr <mohandsakr69@gmail.com>
2020-09-28 18:03:11 +0200

    Create Reverse String.cpp

---
+++ Reverse String.cpp
@@ -0,0 +1,15 @@
+
+/*
+problem link :https://leetcode.com/explore/interview/card/top-interview-questions-easy/127/strings/879/
+problem name:  Reverse String
+status: accepted
+author : moahnd sakr
+** */
+
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        reverse(s.begin(),s.end());
+        
+    }
+};
