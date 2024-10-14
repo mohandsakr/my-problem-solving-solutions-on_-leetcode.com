718781a2478b0cf7e79ca9ac84943b9af5080a24
Mohand Reda Sakr <mohandsakr69@gmail.com>
2024-10-14 22:39:56 +0300

    Create 222. Count Complete Tree Nodes.cpp

---
+++ 222. Count Complete Tree Nodes.cpp
@@ -0,0 +1,36 @@
+/*
+problem link : https://leetcode.com/problems/maximum-depth-of-binary-tree/
+problem name: 222. Count Complete Tree Nodes
+Status: Accepted.
+Author : Mohand sakr.
+** */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+int value=0;
+    int countNodes(TreeNode* root) {
+        
+        traverse(root);
+        return value;
+    }
+
+void traverse(TreeNode* root) {
+        
+        if(root==NULL) return;
+        ++ value;
+        traverse(root->right);
+        traverse(root->left);
+    }
+
+};
